b4cf5bbbf5c5b97d68d7a2f72eb2a1c757203d37
Doctor Eleven <doctoreleven@2403-580d-7f62-0-a4ee-5b54-b0a7-5dc4.ip6.aussiebb.net>
2024-04-21 13:38:29 +0930

    Update

--- website.sh
+++ website.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-#echo "# ryba.ren" >> README.md
-#git init
-#git add README.md
-#git commit -m "first commit"
-#git branch -M main
-#git remote add origin https://github.com/renspringlea/ryba.ren.git
-git add . && git push -u origin main
+bundle exec jekyll build
+#cd _site
+git pull
+git add .
+git commit -m "Update"
+git push
