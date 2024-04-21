7eee0d701a6ec62eb0e5cf1905ce7586b05e70c6
Doctor Eleven <doctoreleven@2403-580d-7f62-0-a4ee-5b54-b0a7-5dc4.ip6.aussiebb.net>
2024-04-21 13:12:55 +0930

    Initial GitHub pages site with Jekyll

---
+++ website.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+git init
+git add .
+git commit -m 'Initial GitHub pages site with Jekyll'
+git remote add origin https://github.com/renspringlea/ryba.ren.git
+git push -u origin main
